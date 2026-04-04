a51b5cd6d6053f60a47704f4c3b7cb1d71c6e41c
Chclt <realchclt@gmail.com>
2026-04-05 01:44:45 +0800

    chore: load Devtools component conditionally based on environment

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -3,13 +3,15 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Devtools from "./components/Devtools";
 import "./index.css";
 import router from "./lib/router";
 
 const rootEl = document.getElementById("root");
 if (rootEl) {
 	const root = ReactDOM.createRoot(rootEl);
+	const Devtools = import.meta.env.DEV
+		? (await import("./components/Devtools")).default
+		: () => null;
 	root.render(
 		<React.StrictMode>
 			<QueryClientProvider client={queryClient}>
