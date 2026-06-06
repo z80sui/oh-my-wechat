30ffb8534d42441d76bfbd4eb15f1244f5df006c
Chclt <realchclt@gmail.com>
2026-06-07 00:43:22 +0800

    fix: type error

--- apps/web/src/components/message/image-message/image-message-default.tsx
+++ apps/web/src/components/message/image-message/image-message-default.tsx
@@ -11,66 +11,66 @@ import { useChatMediaCarouselContext } from "@/routes/$accountId/chat/$chatId/-c
 import type { ImageMessageProps } from "./types.ts";
 
 export function ImageMessageDefault({ message, ...props }: ImageMessageProps) {
-  const { accountId } = useAccount();
+	const { accountId } = useAccount();
 
-  const { ref: imageRef, inViewport } = useInViewport();
+	const { ref: imageRef, inViewport } = useInViewport();
 
-  const { data: image } = useQuery({
-    ...MessageImageQueryOptions({
-      account: { id: accountId },
-      chat: { id: message.chat_id },
-      message,
-    }),
-    enabled: inViewport,
-  });
+	const { data: image } = useQuery({
+		...MessageImageQueryOptions({
+			account: { id: accountId },
+			chat: { id: message.chat_id },
+			message,
+		}),
+		enabled: inViewport,
+	});
 
-  return (
-    <div className={cn("rounded-lg overflow-hidden")} {...props}>
-      <ErrorBoundary
-        fallback={
-          <AutoResolutionFallbackImage
-            ref={imageRef}
-            image={image}
-            className={
-              "max-w-[16em] max-h-128 min-w-32 min-h-16 object-contain bg-white"
-            }
-          />
-        }
-      >
-        <ImageMessageCarouselTrigger
-          ref={imageRef}
-          message={message}
-          image={image}
-          className={
-            "max-w-[16em] max-h-128 min-w-32 min-h-16 object-contain bg-white"
-          }
-        />
-      </ErrorBoundary>
-    </div>
-  );
+	return (
+		<div className={cn("rounded-lg overflow-hidden")} {...props}>
+			<ErrorBoundary
+				fallback={
+					<AutoResolutionFallbackImage
+						ref={imageRef}
+						image={image}
+						className={
+							"max-w-[16em] max-h-128 min-w-32 min-h-16 object-contain bg-white"
+						}
+					/>
+				}
+			>
+				<ImageMessageCarouselTrigger
+					ref={imageRef}
+					message={message}
+					image={image}
+					className={
+						"max-w-[16em] max-h-128 min-w-32 min-h-16 object-contain bg-white"
+					}
+				/>
+			</ErrorBoundary>
+		</div>
+	);
 }
 
 function ImageMessageCarouselTrigger({
-  message,
-  image,
-  ref,
-  ...props
+	message,
+	image,
+	ref,
+	...props
 }: {
-  message: Pick<ImageMessageType, "date">;
-  image?: ImageInfo | null;
-  ref?: React.Ref<HTMLImageElement>;
+	message: ImageMessageType;
+	image?: ImageInfo | null;
+	ref?: React.Ref<HTMLImageElement>;
 } & React.ImgHTMLAttributes<HTMLImageElement>) {
-  const { openChatMediaCarousel } = useChatMediaCarouselContext();
+	const { openChatMediaCarousel } = useChatMediaCarouselContext();
 
-  return (
-    <AutoResolutionFallbackImage
-      ref={ref}
-      image={image}
-      {...props}
-      onClick={() => {
-        // TODO: Refactor
-        openChatMediaCarousel(message);
-      }}
-    />
-  );
+	return (
+		<AutoResolutionFallbackImage
+			ref={ref}
+			image={image}
+			{...props}
+			onClick={() => {
+				// TODO: Refactor
+				openChatMediaCarousel(message);
+			}}
+		/>
+	);
 }
